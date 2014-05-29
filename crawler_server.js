c32c13c2f97bd8513c8c07f43349f1d88dbe3c77
Tim Brandin <tim.brandin@gmail.com>
2014-05-30 00:25:34 +0200

    Removed old stuff.

--- crawler_server.js
+++ crawler_server.js
@@ -17,54 +17,6 @@ Meteor.methods({
   }
 });
 
-var v = {};
-_.extend(v, {
-  consume: function(url) {
-    var next = _next();
-    if (!next) {
-      this.stop();
-    }
-    else {
-      console.log(this._id, 'consume: ' + next.url);
-      // _consume(next.url);
-    }
-  },
-
-  start: function(url) {
-    console.log(this._id, 'started');
-
-    var timestamp = (+new Date);
-    PageQueue.upsert({url: this.url}, {$set: {
-        collection: this.collection,
-        claim: this._id,
-        done: false,
-        timestamp: timestamp
-      }
-    });
-
-    this.resume();
-  },
-
-  stop: function() {
-    console.log(this._id, 'stopped\n');
-    Crawlers.upsert({_id: this._id}, {$set: {running: false}});
-    this.running = false;
-    console.log(this);
-    Meteor.clearInterval(this.interval);
-  },
-
-  resume: function() {
-    var self = this;
-    this.consume();
-
-    this.interval = Meteor.setInterval(function() {
-      if (this.running) {
-        self.consume();
-      }
-    }, this.timeout);
-  }
-});
-
 var _next = function() {//done: false,
   PageQueue.findOne({claim: {$exists: false}}, {sort: {timestamp: -1}});
 };
@@ -94,23 +46,12 @@ var _consume = function(url, collection) {
 
 var _parse = function(responseContent) {
   var fut = new Future();
-  var self = this;
+  this.unblock();
+
   (function() {
-
-    // if (responseContent) {
-    //   PageQueue.upsert({url: url}, {$set: {
-    //     content: responseContent,
-    //     done: true
-    //   }, $unset: {
-    //     claim: true
-    //   }});
-    // } else {
-    //   throw new Error(url + ' returned ' + responseContent);
-    // }
-
     $ = cheerio.load(responseContent);
     var links = $('a');
-    _.each(links, function(link, i) {
+    _.each(links, function(link) {
       var $link = $(link);
       var href = $link.attr('href');
       if (href) {
@@ -119,10 +60,8 @@ var _parse = function(responseContent) {
             rootUrl: url
           });
         }
-        console.log(href);
       }
     });
-
     fut.return(links);
   })();
 
