fb61dffbf6d6e986564b9a32b1ae0f461c636d67
Tim Brandin <tim.brandin@gmail.com>
2014-05-30 00:25:50 +0200

    Prepared for next tests.

--- crawler_tests.js
+++ crawler_tests.js
@@ -5,6 +5,17 @@ if (Crawler.Collection) {
     Meteor.publish('crawlers', function() {
       return Crawlers.find();
     });
+    Crawlers.allow({
+      insert: function() {
+        return true;
+      },
+      update: function() {
+        return true;
+      },
+      remove: function() {
+        return true;
+      }
+    })
   }
 }
 
@@ -54,13 +65,43 @@ if (Meteor.isServer) {
       responseContent = res.content;
     }));
 
-    Crawlers._parse(responseContent, expect(function(err, res) {
-      test.isTrue(res, 'Result is empty.');
-      test.equal(typeof res, 'object', 'Result is not a Object');
-      var a = _.first(res);
-      test.equal(typeof a, 'object', 'No link found on http://example.com.');
-      test.equal(typeof a.name, 'string', 'No link found on http://example.com.');
-      test.equal(a.name, 'a', 'Parse returned something else than a link found on http://example.com.');
+    Crawlers._parse(responseContent, expect(function(err, links) {
+      test.isTrue(links, 'Result is empty.');
+      test.equal(typeof links, 'object', 'Result is not a Object');
+      _.each(links, function(link) {
+        test.equal(typeof link, 'object', 'Link is not an object, found on http://example.com.');
+        test.equal(typeof link.name, 'string', 'We cought something else than links, found on http://example.com.');
+        test.equal(link.name, 'a', 'Link is something else than a link, found on http://example.com.');
+      });
     }));
   }]);
+
+
+  testAsyncMulti('Crawler - Crawler can add pages to the Queue.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - One can list pages from the Queue.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler can start.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler can stop.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler can resume.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler can run in different timeout.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler can finish.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Crawler is never crawling pages twice.', [function(test, expect) {
+  }]);
+
+  testAsyncMulti('Crawler - Multiple crawlers can crawl pages simultanously and finish.', [function(test, expect) {
+  }]);
+
 }
